03db8a059b930aa2624726aa6ad7ce0e84fec4fb
paohoalapizco <paolac.alapizco@gmail.com>
2023-01-30 20:15:39 -0800

    :sparkles: Turnede Hangman game into OOP

--- hangman_game/game.py
+++ hangman_game/game.py
@@ -47,17 +47,42 @@ class Hangman:
 
   def choose_level(self):
     choose = chlvl.Choose()
-    self.__text_game = choose.choose_game('Welcome to the game "The Hangman", before to start you need to select a level from the list below')
-    print(self.__text_game)
+    text = choose.choose_game('Welcome to the game "The Hangman", before to start you need to select a level from the list below')
+    self.__text_game = text.upper()
 
   def print_game(self):
     idx = str(self.errors)
     print(draws[idx])
     print(' '.join(self.underscores), '\n')
 
+  def play(self):
+    self.transform_text()
+    hits = 0
+
+    while self.errors < 6:
+      self.print_game()
+      user_input = input('Which letter do you think is part of the text: ').upper()
+
+      if user_input in self.underscores:
+        continue
+
+      occurrences = self.find_letter(user_input)
+      if occurrences > 0 :
+        hits += occurrences
+      else:
+        self.errors += 1
+      
+      if hits == len(self.__text_game.replace(' ', '')) or self.errors == 6: break
+
+
+    self.print_game()
+    if self.errors == 6:
+      print(f'Sorry you lose, the sentense was: {self.__text_game}')
+    else:
+      print(f'Congrats! you won!')
+
 
 if __name__ == '__main__':
-  game = Hangman()
+  game = Hangman() 
   game.choose_level()
-  # game.transform_text()
-  # game.print_game()
+  game.play()
