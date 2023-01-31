197de49db1f6441e87d1995801cdcd2633935137
paohoalapizco <paolac.alapizco@gmail.com>
2023-01-30 20:20:10 -0800

    :recycle: Previous version of The Hangman Game deleted

--- hangman_game/hangman.py
+++ hangman_game/hangman.py
@@ -1,133 +1,88 @@
-from more_itertools import locate 
-from levels import levels as lvl
+from more_itertools import locate
+from draws import draws
+from levels import levels as chlvl
 
-def transform_into_unders(text):
-  text = text.split()
-  letters = []
-
-  for word in text:
-    for letter in word:
-      letters.append(letter)
-    letters.append(" ")
-
-
-  letters.pop() # delete last space.
-  return [ '_' if char != ' ' else ' ' for char in letters]
-
-
-def find_letter(letter, text, game):
-  indexes = list(locate(text, lambda char: char == letter))
-
-  if len(indexes) > 0:
-    for idx in indexes:
-      game[idx] = letter
-
-  return len(indexes), game
-
-
-def draw(errors):
-  hangman_draw = [
-    ['|','-','-','-','-','-','-','-','-','-','-',''],
-    ['|',' ',' ',' ',' ',' ',' ',' ',' ',' ', '|',''],
-    ['|',' ',' ',' ',' ',' ',' ',' ',' ',' ', ' ',''],
-    ['|',' ',' ',' ',' ',' ',' ',' ',' ',' ', ' ',''],
-    ['|',' ',' ',' ',' ',' ',' ',' ',' ',' ', ' ',''],
-    ['|',' ',' ',' ',' ',' ',' ',' ',' ',' ', ' ','']
-  ]
-
-  drawing = ''
-
-  if errors > 0:
-    for error_num in range(1, errors+1):
-      if error_num == 1:
-        hangman_draw[2][10] = 'O'
-      elif error_num == 2:
-        hangman_draw[3][10] = '|'
-      elif error_num == 3:
-        hangman_draw[3][9] = '/'
-      elif error_num == 4:
-        hangman_draw[3][11] = '\\'
-      elif error_num == 5:
-        hangman_draw[4][9] = '/'
-      elif error_num == 6:
-        hangman_draw[4][11] = '\\'
-
-  for row in hangman_draw:
-    drawing += ''.join(row)
-    drawing += '\n'
-
-  print(drawing)
-
-def select_level():
-  text = ''
-  level_options = lvl.get_level_options()
-  print('Welcome to the game "The Hangman", before to start you need to select a level from the list below')
-  
-  while True:
-    user_level = input('   \n'.join(level_options) + '\n==> ')
+class Hangman:
     
-    if user_level.lower() not in level_options:
-      print(f'{user_level} is not in level optios, please trye again')
+  def __init__(self):
+    self.__draws = draws
+    self.__underscores = []
+    self.__text_game = ''
+    self.errors = 0
+
+
+  # Getter Properties
+  @property
+  def draw(self):
+    if self.errors > 6:
+      raise ValueError(f'Error {self.errors} is out of range of hangman game')
+    
+    return self.__draws
+
+  @property
+  def underscores(self):
+    return self.__underscores
+
+  # General Methods
+  def transform_text(self):
+    text = self.__text_game.split()
+    letters = []
+
+    for word in text:
+      for letter in word:
+        letters.append(letter)
+      letters.append(" ")
+
+    letters.pop() # delete last space.
+    self.__underscores = [ '_' if char != ' ' else ' ' for char in letters]
+
+  def find_letter(self, letter):
+    indexes = list(locate(self.__text_game, lambda char: char == letter))
+
+    if len(indexes) > 0:
+      for idx in indexes:
+        self.underscores[idx] = letter
+
+    return len(indexes)
+
+  def choose_level(self):
+    choose = chlvl.Choose()
+    text = choose.choose_game('Welcome to the game "The Hangman", before to start you need to select a level from the list below')
+    self.__text_game = text.upper()
+
+  def print_game(self):
+    idx = str(self.errors)
+    print(draws[idx])
+    print(' '.join(self.underscores), '\n')
+
+  def play(self):
+    self.transform_text()
+    hits = 0
+
+    while self.errors < 6:
+      self.print_game()
+      user_input = input('Which letter do you think is part of the text: ').upper()
+
+      if user_input in self.underscores:
+        continue
+
+      occurrences = self.find_letter(user_input)
+      if occurrences > 0 :
+        hits += occurrences
+      else:
+        self.errors += 1
+      
+      if hits == len(self.__text_game.replace(' ', '')) or self.errors == 6: break
+
+
+    self.print_game()
+    if self.errors == 6:
+      print(f'Sorry you lose, the sentense was: {self.__text_game}')
     else:
-      text = user_level
-      break
-
-  return text
-
-def select_type(level):
-  text = ''
-  type_options = lvl.get_type_of_game(level)
-
-  print(f'Select a type of game from level {level}')
-  
-  while True:
-    user_type = input('   \n'.join(type_options) + '\n==> ')
-    
-    if user_type.lower() not in type_options:
-      print(f'{user_type} is not in type optios from level {level}, please trye again')
-    else:
-      text = user_type
-      break
-    
-  return text
-
-def get_secret_name():
-  level = select_level()
-  user_type = select_type(level)
-
-  secret_name = lvl.get_name(level, user_type)
-
-  return secret_name
-
-def play():
-  text = get_secret_name().upper()
-  chances = 6
-  game = transform_into_unders(text)
-  hits = 0
-
-  while chances > 0:
-    draw(6 - chances)
-    print(' '.join(game))
-    user_input = input('Which letter do you think is part of the text: ').upper()
-
-    if user_input in game:
-      continue
-
-    occurrences, game = find_letter(user_input, text, game)
-    if occurrences == 0 :
-      chances -= 1
-    else:
-      hits += occurrences
-    
-    if hits == len(text.strip()): break
-
-
-  draw(6 - chances)
-  if chances == 0:
-    print(f'Sorry you lose, the sentense was: {text}')
-  else:
-    print(f'Congrats! you won in your {6 - chances} try')
+      print(f'Congrats! you won!')
 
 
 if __name__ == '__main__':
-  play()
+  game = Hangman() 
+  game.choose_level()
+  game.play()
